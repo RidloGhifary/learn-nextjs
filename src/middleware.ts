71c705c89af2e615c7cb22b2e22441c8748db231
RidloGhifary <ridloghfry@gmail.com>
2024-06-17 18:21:19 +0700

    middleware

---
+++ src/middleware.ts
@@ -0,0 +1,18 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+  const isAdmin = false;
+
+  if (request.nextUrl.pathname.startsWith("/home")) {
+    return NextResponse.redirect(new URL("/", request.url));
+  }
+
+  if (request.nextUrl.pathname.startsWith("/dashboard") && !isAdmin) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+}
+
+export const config = {
+  matcher: ["/dashboard/:path*"],
+};
