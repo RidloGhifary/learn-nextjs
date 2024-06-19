785299fc609e7501c48c0cb31e3a73978e13562e
RidloGhifary <ridloghfry@gmail.com>
2024-06-19 16:32:56 +0700

    fix error on render products dashboard

--- src/app/dashboard/product/page.tsx
+++ src/app/dashboard/product/page.tsx
@@ -1,19 +1,18 @@
 "use client";
 
 import { fetcher } from "@/lib/swr/fetcher";
-import { GetDashboardProduct } from "@/services/products";
 import Image from "next/image";
 import useSWR from "swr";
 
 export default function DashboardProduct() {
-  const { data: datas, isLoading } = useSWR("/api", fetcher);
+  const { data: datas, isLoading } = useSWR(`/api`, fetcher);
 
   if (isLoading) return <p className="text-center">Loading...</p>;
 
   return (
     <div className="max-w-6xl mx-auto p-6">
       <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-3">
-        {datas.map((product: any) => (
+        {datas.data.map((product: any) => (
           <div
             key={product.id}
             className="bg-black/5 border border-black/10 rounded-lg cursor-pointer">
@@ -26,6 +25,21 @@ export default function DashboardProduct() {
               className="aspect-auto object-contain w-full h-auto"
             />
             <h2 className="truncate p-4">{product.title}</h2>
+          </div>
+        ))}
+        {datas.products.map((product: any) => (
+          <div
+            key={product.id}
+            className="bg-black/5 border border-black/10 rounded-lg cursor-pointer">
+            <Image
+              src={product.image}
+              alt={product.title}
+              width={200}
+              height={200}
+              loading="lazy"
+              className="aspect-auto object-contain w-full h-auto"
+            />
+            <h2 className="truncate p-4">{product.name}</h2>
           </div>
         ))}
       </div>
