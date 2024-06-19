e5235199ba31e1c0c5464dc93b2c19caf9f4be12
RidloGhifary <ridloghfry@gmail.com>
2024-06-19 11:46:05 +0700

    SEO & metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,22 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Home | NextJS",
+  description: "Build for learning nextjs deeply",
+  authors: [
+    { name: "Ridlo achmad ghifary", url: "https://github.com/RidloGhifary" },
+  ],
+  themeColor: "#8b5cf6",
+  openGraph: {
+    title: "Home | NextJS",
+    description: "Build for learning nextjs deeply",
+  },
+  twitter: {
+    title: "Home | NextJS",
+    description: "Build for learning nextjs deeply",
+  },
+};
+
 export default function Home() {
   return (
     <main>
